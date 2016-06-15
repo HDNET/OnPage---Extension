b7d0e870f227202cc446031d38175ee9349f39e3
René Backhaus <rene.backhaus@hdnet.de>
2016-06-15 12:13:50 +0200

    Updated ext_localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,17 +1,10 @@
 <?php
-<<<<<<< 5851fedfe7b02562c0bb0e52a45b9344b357a91b
-if (!defined ('TYPO3_MODE')) die ('Access denied.');
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    $_EXTKEY
-);
-=======
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'HDNET.'.$_EXTKEY,
-    'Onpage',
+    'onpage_integration',
     array()
 );
->>>>>>> Added BE Module
+
