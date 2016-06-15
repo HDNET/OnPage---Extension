7af0df44f66fd92bb5c646da4c342d7d39c2275d
René Backhaus <rene.backhaus@hdnet.de>
2016-06-15 14:26:35 +0200

    Added autoloader

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,6 +2,13 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
+
+\HDNET\Autoloader\Loader::extLocalconf('HDNET', 'onpage_integration', [
+    'SmartObjects',
+    'TcaFiles'
+]);
+
+
 TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'HDNET.'.$_EXTKEY,
     'onpage_integration',
