feb8cdfd569e8cd5bd7bb5f5222b1c437e4dbc0c
René Backhaus <rene.backhaus@hdnet.de>
2016-06-15 14:25:07 +0200

    Renamed to Configuration

--- Classes/Domain/Repository/ApiKeyRepository.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class ApiKeyRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
-{
-    
-}
---
+++ Classes/Domain/Repository/ConfigurationRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * ConfigurationRepository
+ */
+
+namespace HDNET\OnpageIntegration\Domain\Repository;
+
+/**
+ * Class ConfigurationRepository
+ */
+class ConfigurationRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
+{
+
+}
