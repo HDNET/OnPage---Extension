440cdf50870b606e222fccd61cb22b414325467a
René Backhaus <rene.backhaus@hdnet.de>
2016-06-15 16:17:48 +0200

    Work von Configuration Provider

--- Classes/Provider/Configuration.php
+++ Classes/Provider/Configuration.php
@@ -9,44 +9,7 @@ namespace HDNET\OnpageIntegration\Provider;
 /**
  * Class Configuration
  */
-class Configuration
+class Configuration extends AbstractProvider
 {
 
-    /**
-     * build for testing
-     */
-    public function buildJson()
-    {
-        $json = [
-            'action'         => 'list',
-            'authentication' => [
-                'api_key' => '5075a56ab20757eb73cc032ccf00443b',
-                'project' => 'www.cts-reisen.de',
-            ],
-            'pagination'     => [
-                'limit'  => 1000,
-                'offset' => 0,
-            ],
-            'group'          => [
-                0 => 'inventory_group',
-            ],
-            'sorting'        => [
-                0 => [
-                    'attribute' => 'count',
-                    'direction' => 'DESC',
-                ],
-            ],
-            'functions'      => [
-                0 => [
-                    'name'       => 'count',
-                    'method'     => 'count',
-                    'parameters' => [
-                        0 => [
-                            'attribute' => 'url',
-                        ],
-                    ],
-                ],
-            ],
-        ];
-    }
 }
