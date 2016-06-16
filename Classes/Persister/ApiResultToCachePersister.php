e8919d56edfa13df43be957357791666d0535a8e
Sebastian Sundermann <sebastian.sundermann@hdnet.de>
2016-06-16 11:14:33 +0200

    OPE-5 Persister basic

---
+++ Classes/Persister/ApiResultToCachePersister.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace HDNET\OnpageIntegration\Persister;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Cache\CacheManager;
+
+class ApiResultToCachePersister
+{
+
+    const TAG = 'onpage_extension_deprecated';
+
+    const CACHE_LIFETIME = 86400;
+
+    protected $tags = [self::TAG];
+
+    // Formel für identifier: Page-UID + module_name + headline + index + [graph|table]
+    // Oder anders: UID + alle Array-Keys des API-Calls
+    // darüber sha1
+
+    /**
+     * @param string $data
+     * @param string $identifier
+     */
+    public function persist($data, $identifier)
+    {
+        /** @var CacheManager $cacheManager */
+        $cacheManager = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Cache\\CacheManager');
+        $cache        = $cacheManager->getCache('onpage_extension');
+        $cache->set($this->getIdentifier($identifier), $data, $this->tags, self::CACHE_LIFETIME);
+    }
+
+    /**
+     * @param string $identifier
+     * @return string
+     */
+    protected function getIdentifier($identifier)
+    {
+        return sha1($this->getPageUid() . $identifier);
+    }
+
+    /**
+     * @return int
+     */
+    protected function getPageUid()
+    {
+        return 42;
+    }
+}
+
+
+
