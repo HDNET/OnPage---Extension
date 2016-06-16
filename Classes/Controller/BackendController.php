ab796ac7ee80a249b3f87d1f882a9e6ed929f423
René Backhaus <rene.backhaus@hdnet.de>
2016-06-16 16:38:44 +0200

    Implemented indexAction

--- Classes/Controller/BackendController.php
+++ Classes/Controller/BackendController.php
@@ -2,13 +2,35 @@
 
 namespace HDNET\OnpageIntegration\Controller;
 
+use HDNET\OnpageIntegration\Service\ApiCallService;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 class BackendController extends ActionController
 {
 
+    /**
+     * Represent the index page
+     */
     public function indexAction()
     {
+        $latestCrawl = '';
+        $seoAspects = '';
+        $contentAspects = '';
+        $technicalAspects = '';
 
+        $apiCallService = GeneralUtility::makeInstance(ApiCallService::class);
+
+        $lastCrawlResult = $apiCallService->makeCall($latestCrawl);
+        $seoAspectsResult = $apiCallService->makeCall($seoAspects);
+        $contentAspectsResult = $apiCallService->makeCall($contentAspects);
+        $technicalAspectsResult = $apiCallService->makeCall($technicalAspects);
+
+        $this->view->assignMultiple([
+            'lastCrawl'        => $latestCrawl,
+            'seoAspects'       => $seoAspects,
+            'contentAspects'   => $contentAspects,
+            'technicalAspects' => $technicalAspects,
+        ]);
     }
 }
