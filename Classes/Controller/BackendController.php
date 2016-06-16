c716064d558a5f36ed567666fe5016d444bcc8dd
René Backhaus <rene.backhaus@hdnet.de>
2016-06-16 15:46:20 +0200

    Work on

--- Classes/Controller/BackendController.php
+++ Classes/Controller/BackendController.php
@@ -2,8 +2,6 @@
 
 namespace HDNET\OnpageIntegration\Controller;
 
-use HDNET\OnpageIntegration\Service\DataService;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 class BackendController extends ActionController
@@ -11,7 +9,6 @@ class BackendController extends ActionController
 
     public function indexAction()
     {
-        $dataService = GeneralUtility::makeInstance(DataService::class);
-        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($dataService->getSingleApiCall());
+
     }
 }
