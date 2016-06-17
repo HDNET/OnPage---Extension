0c78c80eaa121b5dbdc2145ec4c5ea7dbf29bba5
René Backhaus <rene.backhaus@hdnet.de>
2016-06-17 11:05:08 +0200

    Clean up BackendController

--- Classes/Controller/BackendController.php
+++ Classes/Controller/BackendController.php
@@ -2,6 +2,7 @@
 
 namespace HDNET\OnpageIntegration\Controller;
 
+use HDNET\OnpageIntegration\Service\ApiCallService;
 use HDNET\OnpageIntegration\Service\DataService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
@@ -12,7 +13,44 @@ class BackendController extends ActionController
     /**
      * Represent the index page
      */
-    public function workOnAction()
+    public function indexAction()
+    {
+        $seoAspects = '';
+        $contentAspects = '';
+        $technicalAspects = '';
+
+        $apiCallService = GeneralUtility::makeInstance(ApiCallService::class);
+
+        $lastCrawlResult = $apiCallService->makeCall($latestCrawl);
+        $seoAspectsResult = $apiCallService->makeCall($seoAspects);
+        $contentAspectsResult = $apiCallService->makeCall($contentAspects);
+        $technicalAspectsResult = $apiCallService->makeCall($technicalAspects);
+
+        $this->view->assignMultiple([
+            'lastCrawl'        => $latestCrawl,
+            'seoAspects'       => $seoAspects,
+            'contentAspects'   => $contentAspects,
+            'technicalAspects' => $technicalAspects,
+        ]);
+    }
+
+    public function detailAction()
+    {
+        $apiCallService = GeneralUtility::makeInstance(ApiCallService::class);
+
+        $graph = $apiCallService->makeCall($graphApi);
+        $table = $apiCallService->makeCall($tableApi);
+
+        $this->view->assignMultiple([
+            'graph' => $graph,
+            'table' => $table
+        ]);
+    }
+
+    /**
+     *
+     */
+    public function keywordAction()
     {
         $dataService = GeneralUtility::makeInstance(DataService::class);
 
