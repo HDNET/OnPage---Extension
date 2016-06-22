27c307c04be70c0d03e9340f41a676c2230d4b8c
René Backhaus <rene.backhaus@hdnet.de>
2016-06-22 16:52:42 +0200

    Refactor BackendController

--- Classes/Controller/BackendController.php
+++ Classes/Controller/BackendController.php
@@ -1,12 +1,18 @@
 <?php
 
+/**
+ * Class BackendController
+ */
+
 namespace HDNET\OnpageIntegration\Controller;
 
 use HDNET\OnpageIntegration\Provider\MetaDataProvider;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
-use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
+/**
+ * Class BackendController
+ */
 class BackendController extends ActionController
 {
 
@@ -33,63 +39,26 @@ class BackendController extends ActionController
     }
 
     /**
-     * @param string $section
-     * @param string $call
+     * Handle the detail pages
+     *
+     * @param $section
+     * @param $call
      */
     public function detailAction($section, $call)
     {
-        $apiCallString = 'zoom_' . $section . '_' .$call . '_table';
-        DebuggerUtility::var_dump($apiCallString);
-
-        #$table = $this->loader->load($apiCallString);
-    }
-
-    /**
-     * Detail Page
-     *
-     * @param string $call
-     */
-    public function seoAction($call)
-    {
-        $apiCallString = 'zoom_' . $call . '_table';
-
+        $apiCallString = 'zoom_' . $section . '_' . $call . '_table';
         $table = $this->loader->load($apiCallString);
+
+        $layout = ucfirst(str_replace('aspects', '', $section));
+
         $this->view->assignMultiple([
             'table'      => $table,
-            'moduleName' => 'SEO Aspekte'
+            'layout'     => $layout
         ]);
     }
 
     /**
-     * @param string $call
-     */
-    public function contentAction($call)
-    {
-        $apiCallString = 'zoom_' . $call . '_table';
-
-        $table = $this->loader->load($apiCallString);
-        $this->view->assignMultiple([
-            'table'      => $table,
-            'moduleName' => 'Inhaltliche Aspekte'
-        ]);
-    }
-
-    /**
-     * @param string $call
-     */
-    public function technicalAction($call)
-    {
-        $apiCallString = 'zoom_' . $call . '_table';
-
-        $table = $this->loader->load($apiCallString);
-        $this->view->assignMultiple([
-            'table'      => $table,
-            'moduleName' => 'Technische Aspekte'
-        ]);
-    }
-
-    /**
-     *
+     * Empty Keyword Page
      */
     public function keywordAction()
     {
