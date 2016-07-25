b8536e20c4a7d4368127322bc66909cf01bb2793
René Backhaus <rene.backhaus@hdnet.de>
2016-07-25 15:38:53 +0200

    Formatted

--- Classes/Loader/ApiResultLoader.php
+++ Classes/Loader/ApiResultLoader.php
@@ -5,16 +5,17 @@
 namespace HDNET\OnpageIntegration\Loader;
 
 use HDNET\OnpageIntegration\Exception\ApiErrorException;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use HDNET\OnpageIntegration\Service\DataService;
 use HDNET\OnpageIntegration\Persister\ApiResultToCachePersister;
+use HDNET\OnpageIntegration\Service\DataService;
 use TYPO3\CMS\Core\Cache\CacheManager;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Class ApiResultLoader
  */
 class ApiResultLoader
 {
+
     /**
      * @var DataService
      */
@@ -28,25 +29,26 @@ class ApiResultLoader
     public function __construct()
     {
         $this->dataService = GeneralUtility::makeInstance(DataService::class);
-        $this->persister   = GeneralUtility::makeInstance(ApiResultToCachePersister::class);
+        $this->persister = GeneralUtility::makeInstance(ApiResultToCachePersister::class);
     }
 
     /**
      * @param string $key
+     *
      * @return array
      */
     public function load($key)
     {
         $cacheId = $this->persister->getIdentifier($key);
 
-        $entry = GeneralUtility::makeInstance(CacheManager::class)->getCache('onpage_extension')->get(
-            $cacheId
-        );
+        $entry = GeneralUtility::makeInstance(CacheManager::class)
+            ->getCache('onpage_extension')
+            ->get($cacheId);
 
         if ($entry === false) {
             try {
                 $entry = $this->dataService->getApiResult($key);
-            }catch (ApiErrorException $e) {
+            } catch (ApiErrorException $e) {
 
             }
             $this->persister->persist($entry, $key);
