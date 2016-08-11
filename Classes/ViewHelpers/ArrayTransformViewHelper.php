003635793f66ff7b1ad1c8b2bbf63dbd06819d13
René Backhaus <rene.backhaus@hdnet.de>
2016-08-11 16:07:34 +0200

    PSR formatted

--- Classes/ViewHelpers/ArrayTransformViewHelper.php
+++ Classes/ViewHelpers/ArrayTransformViewHelper.php
@@ -15,14 +15,14 @@ class ArrayTransformViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\Abstract
      * @var array
      */
     protected $transformedLastCrawl = [
-        'working_page' => false,
+        'working_page'          => false,
         'working_external_link' => false,
-        'working_asset' => false,
-        'working_image' => false,
-        'broken_page' => false,
-        'broken_external_link' => false,
-        'broken_asset' => false,
-        'broken_image' => false
+        'working_asset'         => false,
+        'working_image'         => false,
+        'broken_page'           => false,
+        'broken_external_link'  => false,
+        'broken_asset'          => false,
+        'broken_image'          => false
     ];
 
     /**
@@ -34,9 +34,9 @@ class ArrayTransformViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\Abstract
      */
     public function render($lastCrawl)
     {
-        foreach($lastCrawl as $lastCrawlElement) {
-            foreach($this->transformedLastCrawl as $key => $element) {
-                if($lastCrawlElement['inventory_group'] === $key) {
+        foreach ($lastCrawl as $lastCrawlElement) {
+            foreach ($this->transformedLastCrawl as $key => $element) {
+                if ($lastCrawlElement['inventory_group'] === $key) {
                     $this->transformedLastCrawl[$key] = $lastCrawlElement['count'];
                 }
             }
